61fb2bfc2bcdfd136186683b0d93e251219933d9
Reza Mehdizadeh <66640294+reme2600@users.noreply.github.com>
2022-08-25 15:50:15 +0200

    Add files via upload

---
+++ MM11.m
@@ -0,0 +1,49 @@
+clear
+clc
+
+C0 = 5*10^3;
+I0 = 770000;
+M0 = 64000;
+
+d1= 1*10^6;
+
+[t,y] = ode23s(@MMm2,[0:0.1:100],[C0 ; I0 ; M0]);
+% [ts,ys] = ode23s(@MMm2,[0:0.1:20],[Cp0; I0; M0]);
+% Cp0= ys(end,1)+d1;
+% Cq0= ys(end,2);
+% I0= ys(end,3);
+% M0= ys(end,4);
+% [tm,ym] = ode23s(@MMm2,[20:0.1:100],[Cp0; I0; M0]);
+% 
+% y=[ys; ym];
+% t=[ts; tm];
+
+
+
+plot(t,y(:,1),'r-',t,y(:,2),'k.-',t,y(:,3),'b.-')
+xlabel('Time (days after tumor cells inoculation)');
+ylabel('Cells number (×10^7)');
+legend('Tumor cell','Immune cell','MDSC')
+ylim([0 70000000])
+
+
+hold on
+t = [0 7 13 17 22 27 29];
+C = [5*10^4 5*10^4 2*10^6 5.47*10^6 8.44*10^6 1.75*10^7 2.33*10^7];
+err = [0 0 1.95*10^5 3.9*10^5 3.2*10^5 1.49*10^6 9.5*10^5];
+% errorbar(t,Cp,err,'-o','DisplayName','Proliferative cell-experimental')
+
+t =[0 7 14 30];
+I = [770000 1.93*10^6 2.31*10^6 2.62*10^5];
+err = [0 5.32*10^5 7.94*10^5 60000];
+% errorbar(t,I,err,'-o','DisplayName','Immune cell-experimental')
+
+t =[0 7 14 30];
+M = [64000 414000 2670000 11507000];
+err = [0 235098 330654 636215];
+err = [0 147400 600000 373000];
+% errorbar(t,M,err,'DisplayName','MDSC-experimental')
+
+
+
+figure(1)
---
+++ MMm2.m
@@ -0,0 +1,33 @@
+ function dydt = MMm2(t,y)
+global t1;
+global e;
+global ts;
+
+a1=0.38*10^-1;
+Cmax = 10^9;
+tau = 0.16*10^-7;
+s= 1.3*10^4;
+q= 12*10^-2;
+l = 2.02*10^7;
+n = 4.12*10^-2;
+mue= 4.5*10^-8;
+alpha=2.2*10^-1;
+beta=0.35;
+
+
+a = a1 * y(1) * log(Cmax/(y(1)+1)) - tau * y(1) * y(2); %y1 = C
+
+b = s + y(2) * (q * (y(1)^2)/(l + y(1)^2)) - n * y(2) - mue * y(2) * y(3); %y2 = I
+
+c = alpha * y(1) - beta * y(3); %y3 = MDSC
+
+
+
+%     if mod (floor(t),2)== 1 && t>ts && t<ts+t1
+    if mod (floor(t),2)== 1 && t>21 && t<51   
+      c =(-10*y(3)); 
+
+    end
+    
+dydt = [a; b; c];  
+end
